5c6aee0d52d9c79dcc1fed7e11a29eade02d409c
LakshitaBh <lakshitabhargava96@gmail.com>
2023-12-05 22:31:24 -0500

    Added API documentation

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * The ExpenseTrackerModel class represents the model in the MVC
+ * design pattern. It uses the Observer design pattern to notify
+ * registered listeners of state changes.
+ */
 public class ExpenseTrackerModel {
 
   //encapsulation - data integrity
@@ -13,13 +18,23 @@ public class ExpenseTrackerModel {
 
   // This is applying the Observer design pattern.                          
   // Specifically, this is the Observable class. 
-    
+
+  /**
+   * Constructs an ExpenseTrackerModel object with no transactions,
+   * no matched filter indices, and no registered listeners.
+   */  
   public ExpenseTrackerModel() {
     transactions = new ArrayList<Transaction>();
     matchedFilterIndices = new ArrayList<Integer>();
     listeners = new ArrayList<ExpenseTrackerModelListener>();
   }
 
+  /**
+   * Adds a transaction to the model.
+   *
+   * @param t The transactions to be added to the model
+   * @throws IllegalArgumentException If the transactions list is null
+   */
   public void addTransaction(Transaction t) {
     // Perform input validation to guarantee that all transactions added are non-null.
     if (t == null) {
@@ -31,6 +46,11 @@ public class ExpenseTrackerModel {
     stateChanged();
   }
 
+  /**
+   * Removes a transaction from the model.
+   *
+   * @param t The transactions to be removed from the model
+   */
   public void removeTransaction(Transaction t) {
     transactions.remove(t);
     // The previous filter is no longer valid.
@@ -38,11 +58,25 @@ public class ExpenseTrackerModel {
     stateChanged();
   }
 
+  /**
+   * Returns the unmodifiable list of transactions in the model.
+   * 
+   * @return The unmodifiable list of transactions
+   */
   public List<Transaction> getTransactions() {
     //encapsulation - data integrity
     return Collections.unmodifiableList(new ArrayList<>(transactions));
   }
 
+  /**
+   * Sets the matched filter indices to the given list of indices.
+   * Calling this method will notify all registered listeners of
+   * a state change.
+   *
+   * @param newMatchedFilterIndices The new list of matched filter indices
+   * @throws IllegalArgumentException If the new list of matched filter indices is null
+   *                                 or if any of the indices are out of range
+   */
   public void setMatchedFilterIndices(List<Integer> newMatchedFilterIndices) {
       // Perform input validation
       if (newMatchedFilterIndices == null) {
@@ -59,6 +93,11 @@ public class ExpenseTrackerModel {
       stateChanged();
   }
 
+  /**
+   * Returns the unmodifiable list of matched filter indices.
+   * 
+   * @return The unmodifiable list of matched filter indcices
+   */
   public List<Integer> getMatchedFilterIndices() {
       // For encapsulation, copy out the output list
       List<Integer> copyOfMatchedFilterIndices = new ArrayList<Integer>();
@@ -105,16 +144,33 @@ public class ExpenseTrackerModel {
       return true;
     }
 
+  /**
+   * Returns the number of registered listeners.
+   *
+   * @return The number of registered listeners
+   */
   public int numberOfListeners() {
       // For testing, this is one of the methods.
       return listeners.size();
   }
 
+  /**
+   * Returns true if the given ExpenseTrackerModelListener is
+   * registered for state change events.
+   *
+   * @param listener The ExpenseTrackerModelListener to be checked
+   * @return If the listener is non-null and registered, returns true.
+   *         If not, returns false.
+   */
   public boolean containsListener(ExpenseTrackerModelListener listener) {
       // For testing, this is one of the methods.
       return listeners.contains(listener);
   }
 
+  /**
+   * Notifies all registered listeners of a state change.
+   * This method is called when transactions are added or removed.
+   */
   protected void stateChanged() {
       // For the Observable class, this is one of the methods.
       for (ExpenseTrackerModelListener listener : listeners) {
