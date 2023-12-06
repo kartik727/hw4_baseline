3d86897c8289de1ac4e1c5ee6cf2dadad7d767f5
Kartik Choudhary <kartikchoudh@umass.edu>
2023-12-05 19:41:49 -0500

    Implement methods in model for observer pattern

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -9,6 +9,7 @@ public class ExpenseTrackerModel {
   //encapsulation - data integrity
   private List<Transaction> transactions;
   private List<Integer> matchedFilterIndices;
+  private List<ExpenseTrackerModelListener> listeners;
 
   // This is applying the Observer design pattern.                          
   // Specifically, this is the Observable class. 
@@ -16,6 +17,7 @@ public class ExpenseTrackerModel {
   public ExpenseTrackerModel() {
     transactions = new ArrayList<Transaction>();
     matchedFilterIndices = new ArrayList<Integer>();
+    listeners = new ArrayList<ExpenseTrackerModelListener>();
   }
 
   public void addTransaction(Transaction t) {
@@ -71,28 +73,30 @@ public class ExpenseTrackerModel {
    */   
   public boolean register(ExpenseTrackerModelListener listener) {
       // For the Observable class, this is one of the methods.
-      //
-      // TODO
-      return false;
+      if (listener == null) {
+        return false;
+      }
+      if (listeners.contains(listener)) {
+        return false;
+      }
+      listeners.add(listener);
+      return true;
   }
 
   public int numberOfListeners() {
       // For testing, this is one of the methods.
-      //
-      //TODO
-      return 0;
+      return listeners.size();
   }
 
   public boolean containsListener(ExpenseTrackerModelListener listener) {
       // For testing, this is one of the methods.
-      //
-      //TODO
-      return false;
+      return listeners.contains(listener);
   }
 
   protected void stateChanged() {
       // For the Observable class, this is one of the methods.
-      //
-      //TODO
+      for (ExpenseTrackerModelListener listener : listeners) {
+        listener.update(this);
+      }
   }
 }
